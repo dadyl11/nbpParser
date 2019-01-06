ebf555961e960e919b3a66f9dd5b3896c9657b17
kmisko <kmisko11@gmail.com>
2019-01-06 23:32:42 +0100

    changed DOM class

--- src/main/java/pl/parser/nbp/xmlParser/DOMParser.java
+++ src/main/java/pl/parser/nbp/xmlParser/DOMParser.java
@@ -15,34 +15,43 @@ import org.xml.sax.SAXException;
 public class DOMParser {
 
   public String[] getBuyAndSellRatesFromXmlFile(String path, String currency) {
+
     String[] buyAndSaleRate = new String[2];
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    NodeList nodeList = getNodeListFromXmlFile(path);
+
+    for (int i = 0; i < nodeList.getLength(); i++) {
+      Node nNode = nodeList.item(i);
+      if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+        Element element = (Element) nNode;
+        if (element.getElementsByTagName("kod_waluty").item(0).getTextContent().equals(currency)) {
+          buyAndSaleRate[0] = element.getElementsByTagName("kurs_kupna").item(0).getTextContent();
+          buyAndSaleRate[1] = element.getElementsByTagName("kurs_sprzedazy").item(0).getTextContent();
+          break;
+        }
+      }
+    }
+    return buyAndSaleRate;
+  }
+
+  private NodeList getNodeListFromXmlFile(String path) {
+
+    NodeList nodeList = null;
+
     try (InputStream input = new URL(path).openStream()) {
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
       DocumentBuilder builder = factory.newDocumentBuilder();
       Document document = builder.parse(input);
       document.getDocumentElement().normalize();
 
-      NodeList nodeList = document.getElementsByTagName("pozycja");
+      nodeList = document.getElementsByTagName("pozycja");
 
-      for (int i = 0; i < nodeList.getLength(); i++) {
-        Node nNode = nodeList.item(i);
-        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-          Element element = (Element) nNode;
-          if (element.getElementsByTagName("kod_waluty").item(0).getTextContent().equals(currency)) {
-            buyAndSaleRate[0] = element.getElementsByTagName("kurs_kupna").item(0).getTextContent();
-            buyAndSaleRate[1] = element.getElementsByTagName("kurs_sprzedazy").item(0).getTextContent();
-            break;
-          }
-        }
-      }
-
-    } catch (IOException ex) {
-      ex.printStackTrace();
-    } catch (ParserConfigurationException ex) {
-      ex.printStackTrace();
-    } catch (SAXException ex) {
-      ex.printStackTrace();
+    } catch (ParserConfigurationException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (SAXException e) {
+      e.printStackTrace();
     }
-    return buyAndSaleRate;
+    return nodeList;
   }
 }
