ee9b88986b6682698123ae3995a3156d8456c4b9
kmisko <kmisko11@gmail.com>
2019-01-06 02:57:02 +0100

    added a few more classes, test and dependencies in pom

--- src/main/java/pl/parser/nbp/MainClass.java
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -10,9 +10,12 @@ public class MainClass {
 
   public static void main(String[] args) {
 
-    String currency = args[0];
-    LocalDate startDate = LocalDate.parse(args[1]);
-    LocalDate endDate = LocalDate.parse(args[2]);
+    String currency = "EUR";
+    LocalDate startDate = LocalDate.parse("2013-01-28");
+    LocalDate endDate = LocalDate.parse("2013-01-31");
+//    String currency = args[0];
+//    LocalDate startDate = LocalDate.parse(args[1]);
+//    LocalDate endDate = LocalDate.parse(args[2]);
 
     DOMParser domParser = new DOMParser();
     XmlFilesPathsCollector xmlFilesPathsCollector = new XmlFilesPathsCollector();
